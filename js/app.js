1ffc59c065017cbed600df0bcaad3bcea4aa0cb3
duncanidaho14 <elhadibeddarem@gmail.com>
2022-09-28 17:35:31 +0200

    api rest crud

--- js/app.js
+++ js/app.js
@@ -1,36 +1,20 @@
 require('@babel/register');
+const mysql = require('mysql')
 const Twig = require('twig');
 const bodyParser = require("body-parser");
 const express = require('express');
-const app = express();
 const PORT = 8080;
 const morgan = require('morgan');
 const config = require('../config.json');
 let functions = require('./functions/functions');
-let membres = [
-    {
-        id: 1,
-        name: 'Snoop',
-        age: 50
-    },
-    {
-        id: 2,
-        name: 'Roni',
-        age: 42
-    },
-    {
-        id: 3,
-        name: 'Lino',
-        age: 47
-    },
-    {
-        id: 4,
-        name: 'Zinedine',
-        age: 44
-    }
-];
 
-let MembersRouter = express.Router();
+
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database : 'nodejs'
+})
 
 // Middleware
 // app.use((req, res, next) => {
@@ -38,109 +22,164 @@ let MembersRouter = express.Router();
 //     console.log('URL : ' + req.url);
 // });
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-twig = Twig.twig;
-
-
-app.get('/api/v5/members/all', (req, res) => {
-    if (req.query.max != undefined && req.query.max > 0) {
-        res.json((functions.success(membres.slice(0, req.query.max))));
-    } else if (req.query.max != undefined) {
-        res.json(functions.error('Wrong max value'))
+db.connect((err) => {
+    
+    if (err) {
+        console.log(err.message)
     } else {
-        res.json((functions.success(membres)));
-    }
-})
+        const app = express();
+        let MembersRouter = express.Router();
 
-MembersRouter.route('/:id')
-    // Récupére un membre avec son id
-    .get( (req, res) => {
-        let index = getIndex(req.params.id); // Affiche sous format json l'id présent dans l'objet  
-        if(typeof(index) == 'string') {
-            res.json(functions.error(index));
-        } else {
-            res.json(functions.success(membres[index]))
-        }
-    })
- 
-    .post( (req, res) => {
-        
-        if (req.body.name) {
-            let same = false;
-            for (let i = 0; i < membres.length; i++) {
-                if(membres[i].name = req.body.name) {
-                    res.json(functions.error('name already taken'));
-                    same = true;
-                    break;
+        app.use(morgan('dev'));
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(bodyParser.json());
+        twig = Twig.twig;
+
+        console.log('Connect to mysql')
+
+        MembersRouter.route('/')
+
+            //Récupère tous les membres
+            .get((req, res) => {
+                if(req.query.max != undefined && req.query.max > 0) {
+                    db.query('SELECT * FROM members LIMIT 0, ?', [req.query.max], (err, result) => {
+                        if(err) {
+                            res.json(functions.error(err.message))
+                        } else {
+                            res.json(functions.success(result))
+                        }
+                    })
+                    
+                } else if (req.query.max != undefined) {
+                    res.json(functions.error('Wrong max value'))
+                } else {
+                    db.query('SELECT * FROM members', (err, result) => {
+                        if(err) {
+                            res.json(functions.error(err.message))
+                        } else {
+                            res.json(functions.success(result))
+                        }
+                    })
                 }
-            }
-            let membre = {
-                id: createID,
-                name: req.body.name,
-                age: req.body.age
-            };
-            
-            membres.push(membre);
-            res.json(functions.success(membre));
-        } else {
-            res.json(functions.error("Pas de valeurs qu'est ce que tu fou ? "))
-        }
-    })
+            })
 
+        MembersRouter.route('/:id')
+            // Récupére un membre avec son id
+            .get( (req, res) => {
+                db.query('SELECT * FROM members WHERE id=?', [req.params.id], (err, result) => {
+                    if(err) {
+                        res.json(functions.error(err.message))
+                    } else {
+                        if(result[0] != undefined) {
+                            res.json( functions.success(result))
+                        } else {
+                            res.json(functions.error('wrong id value'))
+                        }
+                    }
+                })
+            })
+        
+            .post( (req, res) => {
+                
+                if (req.body.name) {
+                    db.query('SELECT * FROM members WHERE name=? AND email=? AND age=?', [req.body.name, req.body.email, req.body.age], (err, result) => {
+                        if (err) {
+                            res.json(functions.error('name already taken'))
+                        } else {
+                            if(result[0] != undefined) {
+                                res.json(functions.error('name already taken'))
+                            } else {
+                                db.query('INSERT INTO members(name, email, age) VALUES(?, ?, ?)', [req.body.name, req.body.email, req.body.age], (err, result) => {
+                                    if (err) {
+                                        res.json(functions.error(err.message))
+                                    } else {
+                                        db.query('SELECT * FROM members WHERE name=?', [req.body.name, req.body.email, req.body.age], (err, result) => {
+                                            if(err) {
+                                                res.json(functions.error(err.message))
+                                            } else {
+                                                res.json(functions.success({
+                                                    id: result[0].id,
+                                                    name: result[0].name
+                                                }))
+                                            }
+                                        })
+                                    }
+                                })
+                            }
+                        }
+                    })
+                } else {
+                    res.json(functions.error("Pas de valeurs qu'est ce que tu fou ? "))
+                }
+            })
 
+            .put( (req, res) => {
 
-    .put( (req, res) => {
-        let index = getIndex(req.params.id);
-
-        if (typeof(index) == 'string') {
-            res.json(functions.error(index));
-        } else {
-            let same = false;
-            for (let i = 0; i < membres.length; i++) {
-                if (req.body.name == membres[i].name && req.params.id != membres[i].id) {
-                    same = true;
-                    break;
+                if (req.body.name) {
+                    db.query('SELECT * FROM members WHERE id=?', [req.params.id], (err, result) => {
+                        if (err) {
+                            res.json(functions.error(err.message))
+                        } else {
+                            if (result[0] != undefined) {
+                                db.query('SELECT * FROM members WHERE name=? AND id != ?', [req.body.name, req.params.id], (err, result) => {
+                                   if (err) {
+                                        res.json(functions.error(err.message))
+                                   } else {
+                                        if (result[0] != undefined) {
+                                            res.json(functions.error('kiff kiff'))
+                                        } else {
+                                            db.query('UPDATE members SET name=?, email=?, age=? WHERE id=?', [req.body.name, req.body.email, req.body.age, req.params.id], (err, result) => {
+                                                if (err) {
+                                                    res.json(functions.error(err.message))
+                                                } else {
+                                                    res.json(functions.success(true))
+                                                }
+                                            })
+                                        }
+                                   }
+                                })
+                            } else {
+                                res.json(functions.error('Mauvais id'))
+                            }
+                        }
+                    })
+                } else {
+                    res.json(functions.error('pas de nom de valeur bordel'))
                 }
                 
-            }
+            })
 
-            if(same) {
-                res.json(functions.error('le même'))
-            } else {
-                membres[index].name = req.body.name;
-                res.json(functions.success(true));
-            }
-        }
-    })
+            .delete( (req, res) => {
+                db.query('SELECT * FROM members WHERE id=?', [req.params.id], (err, result) => {
+                    if(err) {
+                        res.json(functions.error(err.message))
+                    } else {
+                        if(result[0] != undefined) {
+                            db.query('DELETE FROM members WHERE id=?', [req.params.id], (err, result) => {
+                                if(err) {
+                                    res.json(functions.error(err.message))
+                                } else {
+                                    res.json(functions.success(true))
+                                }
+                            })
+                        } else {
+                            res.json(functions.error('wrong id value'))
+                        }
+                    }
+                })
+            })
 
-    .delete( (req, res) => {
-        let index = getIndex(req.params.id);
+            app.use(config.rootApi, MembersRouter)
 
-        if(typeof(index) == 'string') {
-            res.json(functions.error('same name'))
-        } else {
-            membres.splice(index, 1);
-            res.json(functions.success(membres));
-        }
-    })
-
-app.use(config.rootApi, MembersRouter)
-
-app.listen(config.port, () => {
+    app.listen(config.port, () => {
         console.log('serveur démarré sur le port ' + config.port);
     });
 
-function getIndex(id) {
-    for (let i = 0; i < membres.length; i++) {
-        if (membres[i].id == id) {
-            return i;
-        } 
-    }
-    return 'mauvais id';
-}
+    // app.get('/index', (req, res) => {
+    //     res.sendFile(__dirname+'/../public/index.html')
+    // })
+    }    
+})
 
-function createID() {
-    return membres[membres.length - 1 ].id + 1;
-}
+
+
